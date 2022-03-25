8d06547473326d2009cc8bcd320571946ff4c24d
muhammadheryan <muhammadheryan@ymail.com>
2022-03-25 09:23:44 +0700

    News redis

--- application/news.go
+++ application/news.go
@@ -1,13 +1,31 @@
 package application
 
 import (
+	"encoding/json"
+	"fmt"
 	"news-dd/config"
 	"news-dd/domain"
+	"news-dd/infrastructure/helper"
 	"news-dd/infrastructure/persistence"
+	"time"
 )
 
 // GetNews returns a tags by id
 func GetNews(id int) (*domain.News, error) {
+	client := config.ConnectRedis()
+	redisKey := fmt.Sprintf("news:%d", id)
+	defer client.Close()
+
+	resultRedis, err := client.Get(redisKey).Result()
+	if err == nil {
+		var result *domain.News
+		json.Unmarshal([]byte(resultRedis), &result)
+		if err != nil {
+			return nil, err
+		}
+		return result, nil
+	}
+
 	conn, err := config.ConnectDB()
 	if err != nil {
 		return nil, err
@@ -15,7 +33,12 @@ func GetNews(id int) (*domain.News, error) {
 	defer conn.Close()
 
 	repo := persistence.NewNewsRepositoryWithRDB(conn)
-	return repo.Get(id)
+	result, err := repo.Get(id)
+
+	redisValue := helper.StructToJson(result)
+	client.Set(redisKey, redisValue, time.Hour*12)
+
+	return result, err
 }
 
 // GetAllTags return all news
@@ -44,6 +67,10 @@ func AddNews(p domain.News) error {
 
 // RemoveNews do remove news by id
 func RemoveNews(id int) error {
+	client := config.ConnectRedis()
+	redisKey := fmt.Sprintf("news:%d", id)
+	defer client.Close()
+
 	conn, err := config.ConnectDB()
 	if err != nil {
 		return err
@@ -51,11 +78,19 @@ func RemoveNews(id int) error {
 	defer conn.Close()
 
 	repo := persistence.NewNewsRepositoryWithRDB(conn)
-	return repo.Remove(id)
+	err = repo.Remove(id)
+	if err == nil {
+		client.Del(redisKey)
+	}
+	return err
 }
 
 // UpdateNews do remove news by id
 func UpdateNews(p domain.News, id int) error {
+	client := config.ConnectRedis()
+	redisKey := fmt.Sprintf("news:%d", id)
+	defer client.Close()
+
 	conn, err := config.ConnectDB()
 	if err != nil {
 		return err
@@ -64,6 +99,10 @@ func UpdateNews(p domain.News, id int) error {
 
 	repo := persistence.NewNewsRepositoryWithRDB(conn)
 	p.Id = id
+	err = repo.Update(&p)
+	if err == nil {
+		client.Del(redisKey)
+	}
 
-	return repo.Update(&p)
+	return err
 }
